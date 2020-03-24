556f82643c73a9a97ff1c4f6d8bee74b9670beaa
zhangxinyu <2252993114@qq.com>
2020-03-24 16:06:29 +0800

    修改后提交

--- src/main/java/com/bw/domain/Hello.java
+++ src/main/java/com/bw/domain/Hello.java
@@ -3,6 +3,8 @@ package com.bw.domain;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("helloword");
+		System.out.println("helloword1");
+		System.out.println("helloword2");
+		System.out.println("helloword3");
 	}
 }
